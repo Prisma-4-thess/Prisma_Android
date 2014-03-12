de723b383e86d1f111d6b81d6eecd4ae56d7b6ec
PuR3v1L <flojim4@gmail.com>
2014-03-12 21:18:20 +0200

    v0.11b Changelog: - Created welcome activity

---
+++ prisma/src/main/java/com/spydi2kood/prisma/WelcomeActivity.java
@@ -0,0 +1,11 @@
+package com.spydi2kood.prisma;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by jim on 12/3/2014.
+ */
+public class WelcomeActivity extends ActionBarActivity{
+
+
+}
